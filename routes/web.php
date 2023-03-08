5584c5eda3d74ed6a1d652ca7db8d39fe5d6e0b0
JebaAfia <jeba.afiakhatun@gmail.com>
2023-03-08 22:00:12 +0600

    trying to imolement custom middleware

--- routes/web.php
+++ routes/web.php
@@ -11,23 +11,28 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/users', [UserController::class, 'users'])->name('users');
-Route::get('/user/delete/{id}', [UserController::class, 'delete']);
-Route::get('/user/edit/{id}', [UserController::class, 'showName']);
-Route::post('/user/update', [UserController::class, 'update'])->name('user-update');
+Route::group(['middleware'=>"web"], function(){
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/loans', [LoanController::class, 'loans'])->name('loans');
-Route::get('/loan/insert', [LoanController::class, 'index'])->name('loan-insert');
-Route::post('/loan/insert', [LoanController::class, 'store']);
-Route::get('/loan/delete/{id}', [LoanController::class, 'delete']);
-Route::get('/loan/edit/{id}', [LoanController::class, 'showName']);
-Route::post('/loan/update', [LoanController::class, 'update'])->name('loan-update');
+    Route::get('/users', [UserController::class, 'users'])->name('users');
+    Route::get('/user/delete/{id}', [UserController::class, 'delete']);
+    Route::get('/user/edit/{id}', [UserController::class, 'showName']);
+    Route::post('/user/update', [UserController::class, 'update'])->name('user-update');
 
-Route::get('/deposits', [DepositController::class, 'deposits'])->name('deposits');
-Route::get('/deposit/insert/{id?}', [DepositController::class, 'index'])->name('deposit-insert');
-Route::post('/deposit/insert', [DepositController::class, 'store']);
-Route::get('/deposit/delete/{id}', [DepositController::class, 'delete']);
-Route::get('/deposit/edit/{id}', [DepositController::class, 'showName']);
-Route::post('/deposit/update', [DepositController::class, 'update'])->name('deposit-update');
+    Route::get('/loans', [LoanController::class, 'loans'])->name('loans');
+    Route::get('/loan/insert', [LoanController::class, 'index'])->name('loan-insert');
+    Route::post('/loan/insert', [LoanController::class, 'store']);
+    Route::get('/loan/delete/{id}', [LoanController::class, 'delete']);
+    Route::get('/loan/edit/{id}', [LoanController::class, 'showName']);
+    Route::post('/loan/update', [LoanController::class, 'update'])->name('loan-update');
+
+    Route::get('/deposits', [DepositController::class, 'deposits'])->name('deposits');
+    Route::get('/deposit/insert/{id?}', [DepositController::class, 'index'])->name('deposit-insert');
+    Route::post('/deposit/insert', [DepositController::class, 'store']);
+    Route::get('/deposit/delete/{id}', [DepositController::class, 'delete']);
+    Route::get('/deposit/edit/{id}', [DepositController::class, 'showName']);
+    Route::post('/deposit/update', [DepositController::class, 'update'])->name('deposit-update');
+});
+
+
